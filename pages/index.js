710013055bf808172ae1f2d03dc48b3a9afba34e
Jack Murphy <signal.space@pm.me>
2021-09-02 13:37:06 -0500

    header

--- pages/index.js
+++ pages/index.js
@@ -8,16 +8,43 @@ export default function Home() {
       <Head>
         <title>Shared Frequencies</title>
         <meta name="description" content="Shared Frequencies" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
 
       <main className={styles.main}>
-        <div className={styles.logo}>
-          <Image src="/logo.png" alt="Shared Frequencies Logo" width={236} height={111} />
+        <header className={styles.header}>
+          <div className={styles.logo}>
+            <Image src="/logo.png" alt="Shared Frequencies Logo" width={236} height={111} />
+          </div>
+
+          <div className={styles.oval}></div>
+        </header>
+
+
+        <div>
+          <div className="twitch">
+            <div className="twitch-video">
+              <iframe
+                  src="https://player.twitch.tv/?channel=sharedfrequenciesradio&parent=www.sharedfrequencies.com&parent=sharedfrequencies.com"
+                  frameBorder="0"
+                  scrolling="no"
+                  allowFullScreen="true"
+                  height="100%"
+                  width="100%">
+              </iframe>
+            </div>
+            <div className="twitch-chat d-none d-lg-block">
+              <iframe
+                  frameBorder="0"
+                  scrolling="no"
+                  src="https://www.twitch.tv/embed/sharedfrequenciesradio/chat?darkpopout&parent=www.sharedfrequencies.com&parent=sharedfrequencies.com"
+                  height="100%"
+                  width="100%">
+              </iframe>
+            </div>
+          </div>
         </div>
 
-        <div className={styles.oval}>
-        </div>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
