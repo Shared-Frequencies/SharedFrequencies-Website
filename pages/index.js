5e497de03b98c3133294c1f1349df897cae56937
Jack Murphy <signal.space@pm.me>
2021-09-02 15:05:19 -0500

    removed grid added button

--- pages/index.js
+++ pages/index.js
@@ -11,14 +11,15 @@ export default function Home() {
         <link rel="icon" href="/favicon.png" />
       </Head>
 
+      <header className={styles.header}>
+        <Image className={styles.logo} src="/logo.png" alt="Shared Frequencies Logo" width={236} height={111} />
+        <div className={styles.headerSpacer}></div>
+        <div className={styles.oval}>
+            <Image className={styles.playButton} src="/smallPlay.png" alt="play button" width={25} height={25}></Image>
+        </div>
+      </header>
+
       <main className={styles.main}>
-        <header className={styles.header}>
-          <Image className={styles.logo} src="/logo.png" alt="Shared Frequencies Logo" width={236} height={111} />
-
-          <div className={styles.oval}></div>
-        </header>
-
-
         <div>
           <div className="twitch">
             <div className="twitch-video">
@@ -43,36 +44,6 @@ export default function Home() {
           </div>
         </div>
 
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
 
       <footer className={styles.footer}>
