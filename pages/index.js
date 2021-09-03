992751232cbac9ff8ba0ae7006d4329fb1c4f860
Jack Murphy <signal.space@pm.me>
2021-09-03 18:49:43 -0500

    marquee

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,14 @@ import styles from '../styles/Home.module.css'
 import Head from 'next/head'
 import Image from 'next/image'
 
+export default function Home({ nowPlaying, schedule }) {
 
-export default function Home() {
-    let twitch = ''
+    const sched = Object.values(schedule)
+        .flat()
+        .splice(0, Object.values(schedule).flat().length-1)
+        .map((show) => JSON.stringify({id: show.id, name: show.name, time: show.starts}))
 
+    console.log(sched)
   return (
     <div className={styles.container}>
       <Head>
@@ -18,35 +22,31 @@ export default function Home() {
         <Image className={styles.logo} src="/logo.png" alt="Shared Frequencies Logo" width={236} height={111} />
         <div className={styles.oval}>
             <img className={styles.playButton} src="/smallPlay.png" alt="play button" width={25} height={25} />
+            <marquee className={styles.nowPlaying}>{nowPlaying !== null ? nowPlaying.name : 'Shared Frequencies Radio'}</marquee>
         </div>
       </header>
 
       <main className={styles.main}>
-        <div>
-          <div className="twitch">
-            <div className="twitch-video">
-              <iframe
-                  src="https://player.twitch.tv/?collection=38qoa9pThxWR5A&video=462690692&parent=sharedfrequencies.live"
-                  frameBorder="0"
-                  scrolling="no"
-                  parent="sharedfrequencies.live"
-                  allowFullScreen="true"
-                  height="378"
-                  width="620"
-                  >
-              </iframe>
-            </div>
-            <div className="twitch-chat d-none d-lg-block">
-              <iframe
-                  frameBorder="0"
-                  scrolling="no"
-                  src="https://www.twitch.tv/embed/sharedfrequenciesradio/chat?darkpopout&parent=sharedfrequencies.live"
-                  height="378"
-                  width="620">
-              </iframe>
-            </div>
-          </div>
-        </div>
+          <iframe
+              className={styles.twitchVideo}
+              src="https://player.twitch.tv/?collection=38qoa9pThxWR5A&video=462690692&parent=localhost"
+              frameBorder="0"
+              scrolling="no"
+              parent="sharedfrequencies.live"
+              allowFullScreen="true"
+              height="378"
+              width="100%"
+          >
+          </iframe>
+          <iframe
+              className={styles.twitchChat}
+              frameBorder="0"
+              scrolling="no"
+              src="https://www.twitch.tv/embed/sharedfrequenciesradio/chat?darkpopout&parent=localhost"
+              height="378"
+              width="100%">
+          </iframe>
+
       </main>
 
       <footer className={styles.footer}>
@@ -54,4 +54,27 @@ export default function Home() {
       </footer>
     </div>
   )
+}
+
+export async function getStaticProps(context) {
+    const nowPlayingRes = await fetch(`https://sharedfrequencies.airtime.pro/api/live-info`)
+    const nowPlayingData = await nowPlayingRes.json()
+    const nowPlayingResult = await nowPlayingData.current
+
+    const scheduleRes = await fetch(`https://sharedfrequencies.airtime.pro/api/week-info`)
+    const scheduleData = await scheduleRes.json()
+    const scheduleResult = await scheduleData
+
+    if (!nowPlayingData) {
+        return {
+            notFound: true,
+        }
+    }
+
+    return {
+        props: {
+            nowPlaying: nowPlayingResult,
+            schedule: scheduleResult
+        }, // will be passed to the page component as props
+    }
 }
