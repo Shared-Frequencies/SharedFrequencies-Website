408ca3a558c35179e31c98998302134635dd144f
Jack Murphy <signal.space@pm.me>
2021-09-16 10:35:26 -0500

    links open in new tab

--- components/Footer.js
+++ components/Footer.js
@@ -3,10 +3,10 @@ import styles from "../styles/Home.module.css";
 export default function Footer () {
     return (
         <footer className={styles.footer}>
-            <a href="https://www.facebook.com/SharedFrequenciesRadio">Facebook</a>
-            <a href="https://www.instagram.com/sharedfrequenciesradio/">Instagram</a>
-            <a href="https://soundcloud.com/sharedfrequenciesradio">Soundcloud</a>
-            <a href="https://twitter.com/shrdfrqncsradio">Twitter</a>
+            <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/SharedFrequenciesRadio">Facebook</a>
+            <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/sharedfrequenciesradio/">Instagram</a>
+            <a target="_blank" rel="noopener noreferrer" href="https://soundcloud.com/sharedfrequenciesradio">Soundcloud</a>
+            <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/shrdfrqncsradio">Twitter</a>
         </footer>
     )
 }
