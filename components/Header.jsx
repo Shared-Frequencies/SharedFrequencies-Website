1ac5958bbb509f76bbaa0f98b644ebbcec8c0a43
Benjamin Randall <bengr35@gmail.com>
2022-06-19 20:56:30 -0500

    fix about title font size

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,6 @@
 import styles from "../styles/Home.module.css";
 import Link from 'next/link'
-import Image from "next/image";
+// import Image from "next/image";
 import PlayPauseToggle from "./PlayPauseToggle";
 import {decode} from "html-entities";
 import useSWR from "swr";
