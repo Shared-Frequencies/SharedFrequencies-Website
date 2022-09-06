fce4a8472b83bb3e748f43bbbe8048aaf1d6aeee
Benjamin Randall <bengr35@gmail.com>
2022-09-06 18:24:04 -0500

    display Show Name instead of File Name

--- components/Header.jsx
+++ components/Header.jsx
@@ -18,7 +18,7 @@ export default function Header () {
             <div className={styles.ovalContainer}>
                 <PlayPauseToggle/>
                 <div className={styles.nowPlaying}>
-                    <p>{data !== undefined ? decode(data.name) : 'Shared Frequencies Radio'}</p>
+                    <p>{data !== undefined ? decode(data.current.name) : 'Shared Frequencies Radio'}</p>
                 </div>
                 <div className={styles.liveIndicator}>
                     <p>LIVE</p>
