24db6c0b6d3090514241c9c5de81b429ae5306b5
John Lemak <lemak91@mac.com>
2023-08-09 22:20:20 -0500

    first commit

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -3,21 +3,23 @@ import Link from 'next/link'
 
 export default function Sidebar() {
     return (
-        <div className={styles.sidebar}>
-            <div className={styles.sidebarContent}>
-                <Link href="/">
-                    <a>LISTEN</a>
-                </Link>
-                <Link href="/residents">
-                    <a>RESIDENTS</a>
-                </Link>
-                <Link href="/aboutUs">
-                    <a>ABOUT US</a>
-                </Link>
-                <Link href="https://www.patreon.com/sharedfrequenciesradio">
-                    <a target="_blank" rel="noopener noreferrer">BLOG</a>
-                </Link>
-            </div>
+      <div className={styles.sidebar}>
+        <div className={styles.sidebarContent}>
+          <Link href="/">
+            <a>LISTEN</a>
+          </Link>
+          <Link href="/residents">
+            <a>RESIDENTS</a>
+          </Link>
+          <Link href="/aboutUs">
+            <a>ABOUT US</a>
+          </Link>
+          <Link href="https://www.patreon.com/sharedfrequenciesradio">
+            <a target="_blank" rel="noopener noreferrer">
+              BLOG
+            </a>
+          </Link>
         </div>
-    )
+      </div>
+    );
 }
