2d111e30f15707dd386130e71bb6d53894851394
Benjamin Randall <bengr35@gmail.com>
2024-12-20 11:18:28 -0600

    please fix this debacle

--- next.config.js
+++ next.config.js
@@ -2,18 +2,10 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['images.ctfassets.net'],
+    unoptimized: true
   },
   generateBuildId: async () => {
     const { v4: uuidv4 } = require('uuid');
-
-    // You can, for example, get the latest git commit hash here
-    return  uuidv4();
-  },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      'react-p5': 'react-p5/dist/react-p5.min.js',
-    };
-    return config;
-  },
+    return uuidv4();
+  }
 }
